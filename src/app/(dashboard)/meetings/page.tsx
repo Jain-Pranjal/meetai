5c64876a240437ad0ed4f18fc9850177f0d559cc
Pranjal Jain <pranjalworkon@gmail.com>
2025-06-24 00:03:42 +0530

    feat: added meeting schema and procedure

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+//SERVER COMPONENT
 
+
+import React, { Suspense } from 'react'
+import { getQueryClient, trpc } from '@/trpc/server'
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
+import { ErrorBoundary } from "react-error-boundary";
+import { MeetingsViewLoading,MeetingsViewError,MeetingsView } from "@/modules/meetings/ui/views/MeetingsView";
 const page = () => {
+
+
+  const queryClient=getQueryClient();
+  // prefetching the data for the meetings view
+  void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({ }));
+
+
+
   return (
-    <div>page</div>
+    <>
+    {/* <MeetingListHeader/> */}
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<MeetingsViewLoading />}>
+        <ErrorBoundary fallback={<MeetingsViewError />}>
+          <MeetingsView />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+
+    </>
   )
 }
-
-export default page
---
+++ src/modules/meetings/ui/views/MeetingsView.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { Loading } from "@/components/Loading";
+import { Error } from "@/components/Error-state";
+
+export const MeetingsView=()=>{
+
+    const trpc= useTRPC();
+    const { data } = useSuspenseQuery(
+        trpc.meetings.getMany.queryOptions({})
+    );
+
+
+    return (
+        <p>{data.items.length} meetings found</p>
+    );
+}
+
+
+
+export const MeetingsViewLoading = () => {
+    return (
+        <Loading title='Loading Meetings' description='Please wait while we load the meetings.' />
+    )
+}
+
+export const MeetingsViewError = () => {
+    return (
+        <Error
+            title="Error loading meetings"
+            description="Please try again later."
+        />
+    )
+}

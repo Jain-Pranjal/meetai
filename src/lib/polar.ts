f471bcf537dfba9e6ca9f7d9924518ed8edcd7b2
Pranjal Jain <pranjalworkon@gmail.com>
2025-07-30 13:26:49 +0530

    fix: added the prod/dev env

--- src/lib/polar.ts
+++ src/lib/polar.ts
@@ -1,8 +1,9 @@
 import {Polar} from "@polar-sh/sdk";
-
 export const polarClient = new Polar({
-    accessToken: process.env.POLAR_ACCESS_TOKEN_PROD!,
-    server:"production" //use sandbox for development
+    accessToken: process.env.NEXT_PUBLIC_PRODUCTION === 'true' 
+        ? process.env.POLAR_ACCESS_TOKEN_PROD!
+        : process.env.POLAR_ACCESS_TOKEN_DEV!,
+    server: process.env.NEXT_PUBLIC_PRODUCTION === 'true' ? "production" : "sandbox"
 });
 
 // the access token is generated for the sandbox environment, 

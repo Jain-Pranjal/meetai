7e002cfcd0b5fa52fdebb08f29fde3399233335a
Pranjal Jain <pranjalworkon@gmail.com>
2025-08-01 20:15:54 +0530

    feat: verification of the email

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,8 +5,31 @@ import { db } from "@/db"; // your drizzle instance
 import { FullSchema } from "@/db/schema"; // your schema definition
 import {polar,checkout,portal} from "@polar-sh/better-auth" 
 import { polarClient } from "./polar";
+import { Resend } from "resend";
+import VerifyEmail from "@/components/emails/verify-email";
+import ForgotPasswordEmail from "@/components/emails/reset-password";
+import { resend } from "@/lib/resend"; 
+
 
 export const auth = betterAuth({
+
+     emailVerification: {
+         sendVerificationEmail: async ({ user, url }) => {
+            await resend.emails.send({
+               from: `${process.env.EMAIL_SENDER_NAME} <${process.env.EMAIL_SENDER_ADDRESS}>`,
+                to: user.email,
+                subject: "Verify your email",
+                react: VerifyEmail({ username: user.name, verifyUrl: url }),
+
+            });
+        },
+        sendOnSignUp: true,
+        expiresIn:3600, // 1 hour in seconds
+
+    },
+
+
+
     socialProviders: { 
         github: { 
             clientId: process.env.GITHUB_CLIENT_ID as string,  
@@ -35,7 +58,20 @@ export const auth = betterAuth({
     ],
 
     emailAndPassword: {  
-        enabled: true
+        enabled: true,
+        requireEmailVerification: true, // require email verification before allowing sign in
+
+    sendResetPassword: async ({user, url}) => {
+      await resend.emails.send({
+       from: `${process.env.EMAIL_SENDER_NAME} <${process.env.EMAIL_SENDER_ADDRESS}>`,
+        to: user.email,
+        subject: "Reset your password",
+        text: `Click the link to reset your password: ${url}`,
+        react: ForgotPasswordEmail({ username: user.name, resetUrl: url, userEmail: user.email }),
+      });
+    },
+
+
     },
 
     database: drizzleAdapter(db, {
